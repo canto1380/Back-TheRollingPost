f4d408a8016a85914d9022a6adec3fd8af6f89f6
canto1380 <atapenalba16@gmail.com>
2021-07-01 23:54:51 -0300

    Cambio a db final

--- src/database.js
+++ src/database.js
@@ -1,7 +1,6 @@
 import mongoose from 'mongoose'
 
-// const url = 'mongodb+srv://rollingpost7a:rolling@cluster0.vvsui.mongodb.net/therollingpost'
-const url = 'mongodb+srv://ata:2203casa@cluster0.b9epm.mongodb.net/therollingpost'
+const url = 'mongodb+srv://rollingpost7a:rolling@cluster0.vvsui.mongodb.net/therollingpost'
 mongoose.connect(url,{
     useNewUrlParser:true,
     useUnifiedTopology:true,
