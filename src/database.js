f4ee2b42af51d6c80b080483859a31805d8af733
canto1380 <atapenalba16@gmail.com>
2021-07-01 23:27:20 -0300

    Cambio de url

--- src/database.js
+++ src/database.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose'
 
 
- const url = 'mongodb+srv://rollingpost7a:rolling@cluster0.vvsui.mongodb.net/therollingpost'
+// const url = 'mongodb+srv://rollingpost7a:rolling@cluster0.vvsui.mongodb.net/therollingpost'
  
 
-// const url = 'mongodb+srv://ata:ata123@cluster0.b9epm.mongodb.net/therollingpost'
+const url = 'mongodb+srv://ata:ata123@cluster0.b9epm.mongodb.net/therollingpost'
 
 mongoose.connect(url,{
     useNewUrlParser:true,
