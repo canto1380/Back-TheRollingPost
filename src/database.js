966bc99f4ff1aa0bee9b381496a6a67facaa11e5
canto1380 <atapenalba16@gmail.com>
2021-07-01 23:51:58 -0300

    probando nueva db 1

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
 // const url = 'mongodb+srv://rollingpost7a:rolling@cluster0.vvsui.mongodb.net/therollingpost'
-const url= 'mongodb+srv://rollingpost7a:rolling@cluster0.vvsui.mongodb.net/therollingpost'
+const url = 'mongodb+srv://ata:2203casa@cluster0.b9epm.mongodb.net/therollingpost'
 mongoose.connect(url,{
     useNewUrlParser:true,
     useUnifiedTopology:true,
