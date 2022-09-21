5243aaf63eb8035751f82e96f18476f882814fce
canto1380 <atapenalba16@gmail.com>
2022-09-21 00:49:04 -0300

    add toure for region panel

--- src/controllers/pais.controller.js
+++ src/controllers/pais.controller.js
@@ -4,30 +4,47 @@ const paisControlador = {};
 /* Nuevo pais */
 paisControlador.nuevoPais = async (req, res) => {
   try {
-    const pais = new Pais(req.body);
-    console.log(pais);
-    await pais.save();
-    res.status(201).json(pais);
+    const agregaPais = await buscarPorNombre(req.body.pais)
+    if(agregaPais !== null) {
+      return res.status(400).json({ mensaje: 'Ya existe el pais ingresado'})
+    } else {
+      const pais = new Pais(req.body);
+      await pais.save();
+      res.status(201).json(pais);
+    }
   } catch (error) {
-    res.status(500).json({ mensaje: "No se pudo agregar el paisss" });
+    res.status(500).json({ mensaje: "No se pudo agregar el pais" });
   }
 };
 
+const buscarPorNombre = async(pais) => {
+  const controlPais = pais[0].toUpperCase() + pais.slice(1)
+  const registroPais = await Pais.findOne({pais: controlPais})
+  return registroPais
+}
+
 /* Lista de paises */
 paisControlador.listarPaises = async (req, res) => {
+  const { page = 1, limit = 10, search = '' } = req.query;
   let order = req.query.order ? req.query.order : "asc";
   let sortBy = req.query.sortBy ? req.query.sortBy : "createdAt";
   try {
-    await Pais.find()
+    const regex = new RegExp(search, 'i')
+    let filters = {
+      pais: regex
+    }
+    const count = await Pais.countDocuments()
+    await Pais.find(filters)
+      .limit(limit * 1)
+      .skip((page - 1) * limit)
       .sort([[sortBy, order]])
-      // .populate( {path: "categoria"})
       .exec((err, paises) => {
         if (err) {
           return res.status(400).json({
             error: "No se puede listar los paises",
           });
         } else {
-          res.status(200).json(paises);
+          res.status(200).json({paises, totalRegister: count, totalPages: Math.ceil(count / limit), currentPage: page});
         }
       });
   } catch (error) {
@@ -37,19 +54,49 @@ paisControlador.listarPaises = async (req, res) => {
 
 /* Lista de paises no eliminados*/
 paisControlador.paisesNoEliminados = async (req, res) => {
+  const { page = 1, limit = 10, search = '' } = req.query;
   let order = req.query.order ? req.query.order : "desc";
   let sortBy = req.query.sortBy ? req.query.sortBy : "createdAt";
   try {
-    await Pais.find( {deleted: false} )
+    const regex = new RegExp(search, 'i')
+    let filters = {
+      deleted: false,
+      pais: regex
+    }
+    const count = await Pais.countDocuments()
+    await Pais.find(filters)
+      .limit(limit * 1)
+      .skip((page - 1) * limit)
       .sort([[sortBy, order]])
-      // .populate( {path: "categoria"})
       .exec((err, paises) => {
         if (err) {
           return res.status(400).json({
             error: "No se puede listar los paises",
           });
         } else {
-          res.json(paises);
+          res.status(200).json({paises, totalRegister: count, totalPages: Math.ceil(count / limit), currentPage: page});
+        }
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+/* Lista de paises no eliminados sin paginacion*/
+paisControlador.paisesNoEliminados1 = async (req, res) => {
+  let order = req.query.order ? req.query.order : "desc";
+  let sortBy = req.query.sortBy ? req.query.sortBy : "createdAt";
+  try {
+    
+    await Pais.find({deleted: false})
+      .sort([[sortBy, order]])
+      .exec((err, paises) => {
+        if (err) {
+          return res.status(400).json({
+            error: "No se puede listar los paises",
+          });
+        } else {
+          res.status(200).json(paises);
         }
       });
   } catch (error) {
@@ -67,11 +114,12 @@ paisControlador.buscarPais = async (req, res) => {
   }
 };
 
+
 paisControlador.byId = (req, res, next, id) => {
   Pais.findById(id).exec((err, pais) => {
     if (err || !pais) {
       return res.status(400).json({
-        error: "No se encontro la pais",
+        error: "No se encontro el pais",
       });
     }
     req.pais = pais;
@@ -95,10 +143,28 @@ paisControlador.eliminarPais = async (req, res) => {
 /* Actualizar pais */
 paisControlador.actualizarPais = async (req, res) => {
   try {
-    await Pais.findByIdAndUpdate(req.params.id, req.body);
-    res.status(200).json({ mensaje: "Pais actualizado" });
+    const editaPais = await buscarPorNombre(req.body.pais)
+    if(editaPais !== null) {
+      res.status(400).json({ mensaje: "Ya existe el pais ingresado" });
+    } else {
+      await Pais.findByIdAndUpdate(req.params.id, req.body);
+      res.status(200).json({ mensaje: "Pais actualizado" });
+    }
   } catch (error) {
-    res.status(404).json({ mensaje: "No se pudo actualizar" });
+    res.status(500).json({ mensaje: "No se pudo actualizar" });
   }
 };
+paisControlador.restaurarPais = async(req, res) => {
+  try {
+    const { id } = req.params
+    console.log(id)
+    const pais = await Pais.findById(id)
+    pais.deleted = false
+    pais.save()
+    res.status(200).json({ Mensaje: 'Pais reestablecido' })
+  } catch (error) {
+    res.status(400).json({ Mensaje: 'No se pudo reestablecer el pais indicado' })
+  }
+}
+
 export default paisControlador;
--- src/controllers/provincia.controller.js
+++ src/controllers/provincia.controller.js
@@ -3,29 +3,56 @@ const provinciaControlador = {};
 
 provinciaControlador.nuevaProvincia = async (req, res) => {
   try {
-    const provincia = new Provincia(req.body);
-    console.log(provincia)
-    await provincia.save();
-    res.status(201).json(provincia);
+    const agregaProvincia = await buscarPorNombre(req.body.provincia);
+    if (agregaProvincia !== null) {
+      return res
+        .status(400)
+        .json({ mensaje: "Ya existe la provincia ingresada" });
+    } else {
+      const provincia = new Provincia(req.body);
+      await provincia.save();
+      res.status(201).json(provincia);
+    }
   } catch (error) {
     res.status(500).json({ mensaje: "No se pudo agregar la provincia" });
   }
 };
 
+const buscarPorNombre = async (provincia) => {
+  const controlProvincia = provincia[0].toUpperCase() + provincia.slice(1);
+  const registroProvincia = await Provincia.findOne({
+    provincia: controlProvincia,
+  });
+  return registroProvincia;
+};
+
 provinciaControlador.listarProvincia = async (req, res) => {
-  let order = req.query.order ? req.query.order : "desc";
+  const { page = 1, limit = 10, search = "" } = req.query;
+  let order = req.query.order ? req.query.order : "asc";
   let sortBy = req.query.sortBy ? req.query.sortBy : "createdAt";
   try {
-    await Provincia.find()
+    const regex = new RegExp(search, "i");
+    let filter = {
+      provincia: regex,
+    };
+    const count = await Provincia.countDocuments();
+    await Provincia.find(filter)
+      .limit(limit * 1)
+      .skip((page - 1) * limit)
       .sort([[sortBy, order]])
-      .populate( {path: "idPais"})
+      .populate({ path: "idPais" })
       .exec((err, provincias) => {
         if (err) {
           return res.status(400).json({
             error: "No se puede listar las provincias",
           });
         } else {
-          res.json(provincias);
+          res.status(200).json({
+            provincias,
+            totalRegister: count,
+            totalPages: Math.ceil(count / limit),
+            currentPage: page,
+          });
         }
       });
   } catch (error) {
@@ -34,29 +61,66 @@ provinciaControlador.listarProvincia = async (req, res) => {
 };
 
 provinciaControlador.provinciasNoEliminadas = async (req, res) => {
-    let order = req.query.order ? req.query.order : "desc";
-    let sortBy = req.query.sortBy ? req.query.sortBy : "createdAt";
-    try {
-      await Provincia.find( {deleted: false} )
-        .sort([[sortBy, order]])
-        .populate( {path: "idPais"})
-        .exec((err, provincias) => {
-          if (err) {
-            return res.status(400).json({
-              error: "No se puede listar las provincias",
-            });
-          } else {
-            res.json(provincias);
-          }
-        });
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const { page = 1, limit = 10, search = "" } = req.query;
+  let order = req.query.order ? req.query.order : "desc";
+  let sortBy = req.query.sortBy ? req.query.sortBy : "createdAt";
+  try {
+    const regex = new RegExp(search, "i");
+    let filters = {
+      deleted: false,
+      provincia: regex,
+    };
+    const count = await Provincia.countDocuments();
+    await Provincia.find(filters)
+      .limit(limit * 1)
+      .skip((page - 1) * limit)
+      .sort([[sortBy, order]])
+      .populate({ path: "idPais" })
+      .exec((err, provincias) => {
+        if (err) {
+          return res.status(400).json({
+            error: "No se puede listar las provincias",
+          });
+        } else {
+          res.status(200).json({
+            provincias,
+            totalRegister: count,
+            totalPages: Math.ceil(count / limit),
+            currentPage: page,
+          });
+        }
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
+/* Lista de provincias no eliminadas sin paginacion*/
+provinciaControlador.provinciasNoEliminadas1 = async (req, res) => {
+  let order = req.query.order ? req.query.order : "desc";
+  let sortBy = req.query.sortBy ? req.query.sortBy : "createdAt";
+  try {
+    
+    await Provincia.find({deleted: false})
+      .sort([[sortBy, order]])
+      .exec((err, provincias) => {
+        if (err) {
+          return res.status(400).json({
+            error: "No se puede listar los provincias",
+          });
+        } else {
+          res.status(200).json(provincias);
+        }
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 provinciaControlador.buscarProvincia = async (req, res) => {
   try {
-    const provincia = await Provincia.findById(req.params.id).populate( {path: 'idPais'} );
+    const provincia = await Provincia.findById(req.params.id).populate({
+      path: "idPais",
+    });
     res.status(200).json(provincia);
   } catch (error) {
     res.status(500).json({ mensaje: "Error al buscar una provincia por ID" });
@@ -77,10 +141,10 @@ provinciaControlador.byId = (req, res, next, id) => {
 
 provinciaControlador.eliminarProvincia = async (req, res) => {
   try {
-    const { id } =req.params
-    const provincia = await Provincia.findById(id)
-    provincia.deleted = true
-    provincia.save()
+    const { id } = req.params;
+    const provincia = await Provincia.findById(id);
+    provincia.deleted = true;
+    provincia.save();
     res.status(200).json({ mensaje: "Provincia eliminada" });
   } catch (error) {
     res.status(404).json({ mensaje: "Error al eliminar la provincia" });
@@ -93,6 +157,20 @@ provinciaControlador.actualizarProvincia = async (req, res) => {
     res.status(200).json({ mensaje: "Provincia actualizada" });
   } catch (error) {
     res.status(404).json({ mensaje: "No se pudo actualizar" });
+  }
+};
+
+provinciaControlador.restaurarProvincia = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const provincia = await Provincia.findById(id);
+    provincia.deleted = false;
+    provincia.save();
+    res.status(200).json({ Mensaje: "Provincia restaurada" });
+  } catch (error) {
+    res
+      .status(400)
+      .json({ Mensaje: "No se pudo restaurar la provincia indicada" });
   }
 };
 export default provinciaControlador;
